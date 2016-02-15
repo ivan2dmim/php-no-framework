3fdebef4a7b30b64b9d53f9a15ff53414155ed09
moretti2fundo <moretti@fundo.ch>
2016-02-15 19:04:49 +0100

    routes

---
+++ src/Routes.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    [
+        'GET',
+        '/hello-world',
+        function () {
+            echo 'Hello World';
+        }
+    ],
+    [
+        'GET',
+        '/another-route',
+        function () {
+            echo 'This works too';
+        }
+    ]
+];
