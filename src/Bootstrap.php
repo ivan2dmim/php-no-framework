eaa287f0131bac0713d012931d019db2b69fdd5d
moretti2fundo <moretti@fundo.ch>
2016-02-17 00:08:23 +0100

    injector

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 namespace Example;
 
+use Http\HttpRequest;
 require __DIR__ . '/../vendor/autoload.php';
 
 error_reporting(E_ALL);
@@ -20,8 +21,10 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
-$response = new \Http\HttpResponse();
+$injector = include('Dependencies.php');
+
+$request = $injector->make('Http\HttpRequest');
+$response = $injector->make('Http\HttpResponse');
 
 $routeDefinitionCallback = function (\FastRoute\RouteCollector $r) {
     $routes = include ('Routes.php');
@@ -47,7 +50,8 @@ switch ($routeInfo[0]) {
         $method = $routeInfo[1][1];
         $vars = $routeInfo[2];
         
-        $class = new $className();
+        $class = $injector->make($className);
+        
         $class->$method($vars);
         break;
 }
--- src/Controllers/Homepage.php
+++ src/Controllers/Homepage.php
@@ -1,11 +1,24 @@
 <?php
 namespace Example\Controllers;
 
+use Http\Response;
+use Http\Request;
+
 class Homepage
 {
 
+    private $request;
+    private $response;
+    
+    public function __construct(Request $request, Response $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
     public function show()
     {
-        echo 'Hello World';
+        $content = '<h1>Hello World</h1>';
+        $content .= 'Hello ' .$this->request->getParameter('name', 'stranger');
+        $this->response->setContent($content);
     }
 }
